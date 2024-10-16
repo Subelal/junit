14bc187fa612d88e753d13a43dcc579fd9d8a467
Subelal PAL <subelalrpal@gmail.com>
2024-10-16 12:55:51 +0530

    added junit project

---
+++ src/main/java/com/junit/bs/JavaOperations.java
@@ -0,0 +1,8 @@
+package com.junit.bs;
+
+public class JavaOperations {
+
+    public int add(int a , int b){
+        return (a+b);
+    }
+}
---
+++ src/test/java/com/junit/bs/JavaOperationsTest.java
@@ -0,0 +1,20 @@
+package com.junit.bs;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Fail.fail;
+
+class JavaOperationsTest {
+
+    @Test
+    public void firstTest(){
+        System.out.println("Last Test ");
+    }
+
+    @Test
+    public void addTest(){
+        System.out.println("First Test");
+    }
+
+}

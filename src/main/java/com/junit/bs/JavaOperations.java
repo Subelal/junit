e98ef37f245dbe0889d885679ace030640ed24f4
Subelal PAL <subelalrpal@gmail.com>
2024-10-16 13:13:14 +0530

    added junit project

--- src/main/java/com/junit/bs/JavaOperations.java
+++ src/main/java/com/junit/bs/JavaOperations.java
@@ -5,4 +5,8 @@ public class JavaOperations {
     public int add(int a , int b){
         return (a+b);
     }
+
+    public int multi(int a,int b){
+        return (a*b);
+    }
 }
--- src/test/java/com/junit/bs/JavaOperationsTest.java
+++ src/test/java/com/junit/bs/JavaOperationsTest.java
@@ -1,18 +1,33 @@
 package com.junit.bs;
 
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class JavaOperationsTest {
 
+    JavaOperations javaOperations;
+
+    @BeforeEach
+    public void init(){
+        javaOperations = new JavaOperations();
+        //initialization
+        System.out.println("initialization is done");
+    }
+
+    @AfterEach
+    public void cleanup(){
+        System.out.println("clean up done");
+    }
     @Test
-    public void firstTest(){
-        System.out.println("Last Test ");
+    public void addTest(){
+        System.out.println("First Test....Using java operation class ");
     }
 
     @Test
-    public void addTest(){
-        System.out.println("First Test");
+    public void multiTest(){
+        System.out.println("Second Test....Using java operation class");
     }
 
 }
